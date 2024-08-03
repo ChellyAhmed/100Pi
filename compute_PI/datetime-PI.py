e9e75f18a2d0d68c1349da38d0d627746bb4249e
franci3 <vela_franco@yahoo.de>
2024-08-03 23:33:02 +0200

    Calculates PI by calculating in the past

---
+++ compute_PI/datetime-PI.py
@@ -0,0 +1,22 @@
+import time
+from datetime import datetime, timedelta
+from test_PI import test_PI
+
+
+def datetimePI():
+    seconds = 365 * 24 * 60 * 60  # Seconds passing in a year
+    milliseconds = 996  # We want to get milliseconds of a year when we know the end result will be smaller than PI
+    step = 0.000001  # then increase the milliseconds in very small steps
+    while True:
+        try:
+            current_day_in_seconds = time.time()
+            epoch_time_a_year_ago = (datetime.today() - timedelta(milliseconds=seconds * milliseconds)).timestamp()
+            pi = (current_day_in_seconds - epoch_time_a_year_ago) / 10000000
+            test_PI(pi)
+            print('Found PI from date %s' % datetime.fromtimestamp(epoch_time_a_year_ago).isoformat())
+            break
+        except AssertionError:
+            milliseconds = milliseconds + step
+
+
+datetimePI()
