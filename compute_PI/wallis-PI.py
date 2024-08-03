973dbc8111a48519f3abd9d578e03292be9c7dd0
franci3 <vela_franco@yahoo.de>
2024-08-03 22:52:18 +0200

    Uses the Wallis Product to calculate PI

---
+++ compute_PI/wallis-PI.py
@@ -0,0 +1,15 @@
+from test_PI import test_PI
+def wallisPI():
+    i = 2
+    approximation = 1.0
+    while True:
+        try:
+            approximation *= (i / (i - 1)) * (i / (i + 1))
+            pi = approximation * 2
+            test_PI(pi)
+            print('PI FOUND AFTER %d ITERATIONS' % i)
+            break
+        except AssertionError:
+            i += 2
+
+wallisPI()
