2b2edbc865940dc823f749075b64dede5c0674b0
franci3 <vela_franco@yahoo.de>
2024-08-03 23:37:26 +0200

    returns pi

--- compute_PI/api-PI.py
+++ compute_PI/api-PI.py
@@ -4,10 +4,12 @@ import json
 
 pi_api = "https://api.pi.delivery/v1/pi?start=0&numberOfDigits=6"
 
+
 def getPI():
     result = urllib.request.urlopen(pi_api)
-    pi = json.load(result)['content']
-    test_PI(float(pi)/100000)
+    pi = float(json.load(result)['content']) / 100000
+    test_PI(pi)
+    return pi
 
 
 getPI()
