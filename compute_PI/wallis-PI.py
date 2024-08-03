8a906b17433fe213158d08aec4605faca9009034
franci3 <vela_franco@yahoo.de>
2024-08-03 22:55:26 +0200

    Refacotring

--- compute_PI/wallis-PI.py
+++ compute_PI/wallis-PI.py
@@ -1,4 +1,6 @@
 from test_PI import test_PI
+
+
 def wallisPI():
     i = 2
     approximation = 1.0
@@ -7,9 +9,10 @@ def wallisPI():
             approximation *= (i / (i - 1)) * (i / (i + 1))
             pi = approximation * 2
             test_PI(pi)
-            print('PI FOUND AFTER %d ITERATIONS' % (i/2))
+            print('PI FOUND AFTER %d ITERATIONS' % (i / 2))
             break
         except AssertionError:
             i += 2
+
 
 wallisPI()
